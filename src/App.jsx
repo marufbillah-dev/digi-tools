bcbf62a0ea9a4ac54bfdd4447077f660855d14e3
marufbillah-dev <marufbillah.dev@gmail.com>
2026-03-30 22:22:16 +0600

    feat: add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import CallToAction from "./components/layout/CallToAction";
+import Footer from "./components/layout/Footer";
 import GetStarted from "./components/layout/GetStarted";
 import Hero from "./components/layout/Hero";
 import Navbar from "./components/layout/Navbar";
@@ -19,6 +20,7 @@ const App = () => {
         <Pricing />
         <CallToAction />
       </main>
+      <Footer />
     </div>
   );
 };
